bde07a5c97cf811a7365962a328f7bd870b4a76b
nbryn <niklasbg@live.dk>
2019-09-04 19:55:57 +0200

    Fixed import error

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import user from "./UserReducer";
 import authors from "./AuthorReducer";
-import genres from "./GenreReducer";
 import books from "./BookReducer";
 
 // Generates function that calls reducers with the part of state selected according to the reducers specific keys
