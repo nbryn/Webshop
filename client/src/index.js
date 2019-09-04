ac091c98a555211d16b3e90eda1e44e454b49d22
nbryn <niklasbg@live.dk>
2019-09-04 18:29:54 +0200

    Deployment issues

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./ServiceWorker";
+import "./Resources/css/styles.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
 
-serviceWorker.unregister();
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+import Reducer from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
+ReactDOM.render(
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
