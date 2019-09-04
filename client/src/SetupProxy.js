0b576a02809c57c374373234a6eac5e8c8353e4a
nbryn <niklasbg@live.dk>
2019-09-04 18:15:12 +0200

    Deployment issues

--- client/src/SetupProxy.js
+++ client/src/SetupProxy.js
@@ -1,5 +1,6 @@
-const proxy = require("http-proxy-middleware");
+//const proxy = require("http-proxy-middleware");
 
-module.exports = function(app) {
+/*module.exports = function(app) {
   app.use(proxy("/webshop", { target: "https://localhost:3001/" }));
 };
+*/
