2b748f412bdcc819eb66dc600893b75016348d43
nbryn <niklasbg@live.dk>
2019-09-05 16:55:39 +0200

    Fixed bug causing the app not to work without Redux DevTools installed

--- client/src/Store.js
+++ client/src/Store.js
@@ -6,26 +6,17 @@ import promiseMiddleware from "redux-promise";
 // Contains the entire state of the application
 
 const initialState = {};
-let Store;
 
-const ReactReduxDevTools =
+//Only for development
+
+/*const ReactReduxDevTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+*/
 
-if (window.navigator.userAgent.includes("Chrome")) {
-  Store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(reduxThunk, promiseMiddleware),
-      ReactReduxDevTools
-    )
-  );
-} else {
-  Store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(reduxThunk, promiseMiddleware))
-  );
-}
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(reduxThunk, promiseMiddleware))
+);
 
-export default Store;
+export default store;
